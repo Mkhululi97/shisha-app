99d9e03033afac7e1e4e11e8e9f36a058757f9d3
Mkhululi97 <71942352+Mkhululi97@users.noreply.github.com>
2022-06-08 00:45:17 +0200

    laptop and desktop

---
+++ content.md
@@ -0,0 +1,33 @@
+## hero text
+
+Buying your well trusted brand of shisha flavour has just got
+better. You can now buy your flavor at the comfort of your place,using the shisha app
+
+## hero secondary text
+
+With the shisha app, you can buy all your shisha material, foil coal and flavor, without leaving your place.The material will be delivered striaght to your place.
+
+## reviews
+
+<!-- MPHOZA -->
+
+Awe jita, its Mphoza, if you’ll always have Mint Cream flavor, you
+and I will be friends, really enjoyed that flavour jita. You keep
+selling that flavor, I’ll keep supporting you.
+
+<!-- MPHO -->
+
+Hey please let me know when you will have Cotton Candy I really
+enjoyed it, was very different from the usual mint that I’m use to.
+
+<!-- LESEDI -->
+
+Awe its Lesedi, I wanted to ask if you always have amaren flavours. Ruby Rush and Cotton Candy treated me well.
+
+## footer text
+
+Ready to start smoking?
+
+## footer-btn-text
+
+Yea lets go
--- script.js
+++ script.js
@@ -2,7 +2,43 @@ const btnMobileNav = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
 btnMobileNav.addEventListener("click", function (e) {
   e.preventDefault();
-  // console.log(btnMobileNav);
-  // console.log(header);
   header.classList.toggle("nav-open");
+});
+
+// STICKY NAVIGATION
+const sectionHeroEl = document.querySelector(".hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    // matches the height of nav-sticky
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
+// SMOOTH SCROLLING ANIMATION
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      header.classList.toggle("nav-open");
+  });
 });
